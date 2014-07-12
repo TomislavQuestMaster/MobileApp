178e96c7a4a8e03c74dcbb25f80080020103e3ad
Tomislav <tomislav.dubravcevic@gmail.com>
2014-07-12 18:07:58 +0200

    initial

---
+++ MobileApp.Mobile/mobileapp.config.js
@@ -0,0 +1,14 @@
+
+// NOTE object below must be a valid JSON
+window.MobileApp = $.extend(true, window.MobileApp, {
+    "config": {
+        "layoutSet": "navbar",
+        "navigation": [
+            {
+                "title": "About",
+                "action": "#About",
+                "icon": "info"
+            }
+        ]
+    }
+});
---
+++ MobileApp.Shared/mobileapp.shared.config.js
@@ -0,0 +1,18 @@
+
+// NOTE object below must be a valid JSON
+window.MobileApp = $.extend(true, window.MobileApp, {
+    "config": {
+        "endpoints": {
+            "db": {
+                "local": "",
+                "production": ""
+            }
+        },
+        "services": {
+            "db": {
+                "entities": {
+                }
+            }
+        }
+    }
+});
